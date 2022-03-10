0e9f123f456790071c617bb005682ba8ed144851
Zhuotang Li <zhuotang.li@sjsu.edu>
2022-03-10 14:00:50 -0800

    new website

---
+++ src/Pages/HomepPage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div id="homepage">
+            <h1 id="header">
+                Zhuotang Li
+            </h1>
+        </div>
+    );
+}
+
+export default HomePage;
